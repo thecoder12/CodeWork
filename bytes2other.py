9f3f763b28904158e33f4263c62d2e8972e243b1
darkwhite <dw@dw-ThinkPad-Edge-E430.(none)>
2016-01-15 10:33:40 +0530

    code to convert bytes(coming from python getsize) to kb/mb/gb......no compromise...denominator is 1024

---
+++ bytes2other.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+
+import os
+
+def convert2size(ss, count):
+    i = 1
+    while( i <= count ):
+        ss = ss/1024
+        i = i + 1
+    return ss
+
+
+def bytes2other( size, size_compare ):
+    para = size_compare[-2:]
+    s = ''
+    size_compare = size_compare[:-2]
+    
+    ### kb onvert
+    if para == 'kb':
+        s = convert2size(int(float(size)),1)
+
+    ## mb convert
+    if para == 'mb':
+        s = convert2size(int(float(size)),2)
+
+    ## gb convert
+    if para == 'gb':
+        s = convert2size(int(float(size)),3)
+
+    if s == int(float(size_compare)):
+        print('SAME<>' + str(size_compare) + '<>' + str(s))
+        return 0
+    else:
+        print('<not same>'+ str(size_compare) + '<>' + str(s))
+        return 1
+
+    
+def getfilesize():
+    size = os.path.getsize('/media/dw/Big Data/VID_20151109_072915482_cut.mp4')
+    bytes2other(size, '727.7mb')
+
+if __name__ == "__main__":
+    #bytes2other('50','50k')
+    getfilesize()
